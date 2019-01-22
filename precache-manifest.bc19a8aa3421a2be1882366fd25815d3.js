5658760b3802e327a2a1e221f48770618735b722
Farzad <farzadb23@yahoo.com>
2019-01-22 00:20:14 -0500

    Updates

---
+++ precache-manifest.bc19a8aa3421a2be1882366fd25815d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "081b9131bebbb80abb6e",
+    "url": "/atlas-panel/static/js/runtime~main.081b9131.js"
+  },
+  {
+    "revision": "16d042fe61b98f6cadfe",
+    "url": "/atlas-panel/static/js/main.16d042fe.chunk.js"
+  },
+  {
+    "revision": "ce0f938b4990457662b7",
+    "url": "/atlas-panel/static/js/1.ce0f938b.chunk.js"
+  },
+  {
+    "revision": "16d042fe61b98f6cadfe",
+    "url": "/atlas-panel/static/css/main.89c54743.chunk.css"
+  },
+  {
+    "revision": "e8738862d6b55400b6bb78b8b4bd3546",
+    "url": "/atlas-panel/index.html"
+  }
+];
